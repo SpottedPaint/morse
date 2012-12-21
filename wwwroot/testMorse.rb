0ba61c0b9058180fd06004b3121689365532cf21
Adam <derangedofarkham@yahoo.co.uk>
2012-12-21 17:35:04 +0000

    more tests preserving spaces on CLI

--- wwwroot/testMorse.rb
+++ wwwroot/testMorse.rb
@@ -17,5 +17,17 @@ class TestMorse < Test::Unit::TestCase
 		assert(!m.english?('--- -- eqrwrq'), 'this should be considered morse')
 	end
  
-
+	def test_Morse_translate_with_spaces_preserved
+		m = Morse.new
+		assert(m.translate('Whats the frequency kenneth') != m.translate(m.translate('Whats the frequency kenneth')), 'input should be same as output when morsed and back from english')
+		assert('Whats the Frequency Kenneth'.downcase == m.translate(m.translate('Whats the frequency kenneth')), 'input should be same as output when morsed and back from english, but the case will be converted to lower')
+		assert('Whats the Frequency Kenneth a'.downcase == m.translate(m.translate('Whats the frequency kenneth ?a')), 'input should be same as output when morsed and back from english, but non alpha numeric characters will be lost')	
+	end
+	
+	def test_Morse_translate_with_sentences
+		m = Morse.new
+		# hmm lots of issues
+		# not really sure how to deal with this
+	
+	end
 end
