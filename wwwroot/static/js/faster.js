de2d62720b8b7286d16b3c7704f010ff840c940e
Toby <Toby@Macintosh.local>
2013-03-25 17:38:19 +0000

    removing stringify and making sure don't translate nothing

--- wwwroot/static/js/faster.js
+++ wwwroot/static/js/faster.js
@@ -9,19 +9,24 @@ var audio = document.getElementsByTagName("audio")[0];
 */
 angle = [0,-1,-2,-3,-2,-1,0,1,2,3,2,1];
 var angleIndex = 0;
-$('#input').keydown( function() {
-	$.ajax({
-		url: '/morse/'+JSON.stringify(this.value).replace(/\?/g, "%5C"),
-		dataType: "json",
-		async: true,           // <-- this is the key
-		success: function (data) {
-			$('.output').html(data.output.trim());
-			//replace(/( ){3}/g,' &nbsp; ')
-			//audio.play();
-        },
-		error: function () { }
-    });
-	rotateSubmarine();
+$('#input').keyup( function() {
+	var englishString = (this.value).replace(/\?/g, "%5C").trim();
+	if(englishString.length > 0) {
+		/* only real strings other wise we are translating nowt which is just weird */
+		$.ajax({
+			url: '/morse/'+englishString,
+			dataType: "json",
+			async: true,           // <-- this is the key
+			success: function (data) {
+				$('.output').html(data.output.trim());
+				//replace(/( ){3}/g,' &nbsp; ')
+				//audio.play();
+			},
+			error: function () { }
+		});
+		rotateSubmarine();
+		
+	}
 });
 
 function rotateSubmarine(){
